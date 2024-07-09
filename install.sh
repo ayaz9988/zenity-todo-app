d9c6244f1dec531ad459720ac0272eb01d494b19
ayaz9988 <ayaz9988@github.com>
2024-07-09 16:44:06 +0300

    add the path adding to .profile

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 $(mkdir -p $HOME/.local/share/applications/)
 $(mkdir -p $HOME/.local/bin/)
 $(mkdir -p $HOME/.icons/)
+$(echo 'export PATH="$HOME/.local/bin:$PATH"' >> $HOME/.profile)
 $(cp MyTodo.desktop $HOME/.local/share/applications/)
 $(cp todo.sh $HOME/.local/bin/)
 $(cp to-do-list.png $HOME/.icons/)
