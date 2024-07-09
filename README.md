c1581105a5830067789105af15bfcedfa63d5598
ayaz9988 <ayaz2001aya@gmail.com>
2024-07-09 13:02:24 +0300

    README.md adjustments

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # zenity-todo-app
 a simple todo app written in bash and using zenity for the gui
+---
+## just do the following and u should see it in your app menu
+``` bash
+    chmod +x install.sh
+    ./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+$(mkdir -p $HOME/.local/share/applications/)
+$(mkdir -p $HOME/.local/bin/)
+$(mkdir -p $HOME/.icons/)
 $(cp MyTodo.desktop $HOME/.local/share/applications/)
 $(cp todo.sh $HOME/.local/bin/)
 $(cp to-do-list.png $HOME/.icons/)
